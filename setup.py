c59af825a2bcfe4412c3ff7bf4ba8633e7066191
david <davidtnfsh@gmail.com>
2017-04-20 17:47:43 +0800

    declare specific version

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ from distutils.core import setup
 setup(
     name = 'DbscanApi',
     packages = ['DbscanApi'],
-    version = '1.1',
+    version = '1.2',
     description = 'A Dbscan api.',
     author = 'davidtnfsh',
     author_email = 'davidtnfsh@gmail.com',
     url = 'https://github.com/udicatnchu/DbscanApi',
-    download_url = 'https://github.com/udicatnchu/DbscanApi/archive/v1.1.tar.gz',
+    download_url = 'https://github.com/udicatnchu/DbscanApi/archive/v1.2.tar.gz',
     keywords = ['dbscan'],
     classifiers = [],
     license='MIT',
     install_requires=[
         'jieba',
-        'numpy',
         'sklearn',
-        'gensim'
+        'numpy==1.12.1',
+        'gensim==2.0.0'
     ],
     zip_safe=True
 )
